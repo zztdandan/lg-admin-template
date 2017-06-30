46d9a2897f14b26d27315ac7edaa8aa487d8352c
Pan <panfree23@gmail.com>
2017-06-30 14:18:16 +0800

    add  meta

---
+++ meta.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+
+      return options.inverse(this);
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "author": {
+      "type": "string",
+      "message": "Author"
+    }
+  },
+  "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+};
