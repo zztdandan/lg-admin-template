a20611fadc49c9f3b4a4bfbd935353e2a6cee3d8
zztdan <zztdandan@gmail.com>
2018-10-14 00:17:21 +0800

    On branch 111  Your branch is up to date with 'origin/111'.  Changes to be committed: 	modified:   build/utils.js 	modified:   build/webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,6 +11,9 @@ const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+var glob = require("glob");
+// 取得相应的页面路径，因为之前的配置，所以是src文件夹下的pages文件夹
+var PAGE_PATH = path.resolve(__dirname, "../src/pages");
 
 function resolve(dir) {
   return path.join(__dirname, "..", dir);
@@ -46,64 +49,85 @@ const webpackConfig = merge(baseWebpackConfig, {
     new MiniCssExtractPlugin({
       filename: utils.assetsPath("css/[name].[contenthash:8].css"),
       chunkFilename: utils.assetsPath("css/[name].[contenthash:8].css")
-    }),].concat(utils.htmlPlugin()).concat([
-    //   // generate dist index.html with correct asset hash for caching.
-    // // you can customize output by editing /index.html
-    // // see https://github.com/ampedandwired/html-webpack-plugin
-    // new HtmlWebpackPlugin({
-    //   filename: path.resolve(__dirname, '../dist/index.html'),
-    //   template: "index.html",
-    //   inject: true,
-    //   favicon: resolve("favicon.ico"),
-    //   title: "vue-admin-template",
-    //   chunks:["index","chunk-libs","chunk-elementUI"],
-    //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeAttributeQuotes: true
-    //     // more options:
-    //     // https://github.com/kangax/html-minifier#options-quick-reference
-    //   }
-    //   // default sort mode uses toposort which cannot handle cyclic deps
-    //   // in certain cases, and in webpack 4, chunk order in HTML doesn't
-    //   // matter anyway
-    // }),
-    new ScriptExtHtmlWebpackPlugin({
-      //`runtime` must same as runtimeChunk name. default is `runtime`
-      inline: /runtime\..*\.js$/
-    }),
-    // keep chunk.id stable when chunk has no name
-    new webpack.NamedChunksPlugin(
-      chunk => {
-      if (chunk.name) {
-        return chunk.name;
-      }
-      const modules = Array.from(chunk.modulesIterable);
-      if (modules.length > 1) {
-        const hash = require("hash-sum");
-        const joinedHash = hash(modules.map(m => m.id).join("_"));
-        let len = nameLength;
-        while (seen.has(joinedHash.substr(0, len))) len++;
-        seen.add(joinedHash.substr(0, len));
-        return `chunk-${joinedHash.substr(0, len)}`;
-      } else {
-        return modules[0].id;
-      }
-    }
-  ),
-    // keep module.id stable when vender modules does not change
-    new webpack.HashedModuleIdsPlugin(),
-    // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, "../static"),
-        to: config.build.assetsSubDirectory,
-        ignore: [".*"]
-      }
-    ])
-  ])
+    })
+  ]
+    .concat(
+     utils.htmlPlugin()
+      // new HtmlWebpackPlugin({
+      //   filename: path.resolve(__dirname, "../dist/index.html"),
+      //   template:  path.resolve(__dirname, "../src/pages/index/index.html"),
+      //   inject: true,
+      //   favicon: resolve("favicon.ico"),
+      //   title: "index",
+      //   chunks: ["index", "chunk-libs", "chunk-elementUI"],
+      //   minify: {
+      //     removeComments: true,
+      //     collapseWhitespace: true,
+      //     removeAttributeQuotes: true
+      //     // more options:
+      //     // https://github.com/kangax/html-minifier#options-quick-reference
+      //   }
+      //   // default sort mode uses toposort which cannot handle cyclic deps
+      //   // in certain cases, and in webpack 4, chunk order in HTML doesn't
+      //   // matter anyway
+      // }),
+      // new HtmlWebpackPlugin({
+      //   filename: path.resolve(__dirname, "../dist/cell.html"),
+      //   template:  path.resolve(__dirname, "../src/pages/cell/cell.html"),
+      //   inject: true,
+      //   favicon: resolve("favicon.ico"),
+      //   title: "cell",
+      //   chunks: ["cell", "chunk-libs", "chunk-elementUI"],
+      //   minify: {
+      //     removeComments: true,
+      //     collapseWhitespace: true,
+      //     removeAttributeQuotes: true
+      //     // more options:
+      //     // https://github.com/kangax/html-minifier#options-quick-reference
+      //   }
+      //   // default sort mode uses toposort which cannot handle cyclic deps
+      //   // in certain cases, and in webpack 4, chunk order in HTML doesn't
+      //   // matter anyway
+      // })
+    )
+    .concat([
+      //   // generate dist index.html with correct asset hash for caching.
+      // // you can customize output by editing /index.html
+      // // see https://github.com/ampedandwired/html-webpack-plugin
+
+      new ScriptExtHtmlWebpackPlugin({
+        //`runtime` must same as runtimeChunk name. default is `runtime`
+        inline: /runtime\..*\.js$/
+      }),
+      // keep chunk.id stable when chunk has no name
+      new webpack.NamedChunksPlugin(chunk => {
+        if (chunk.name) {
+          return chunk.name;
+        }
+        const modules = Array.from(chunk.modulesIterable);
+        if (modules.length > 1) {
+          const hash = require("hash-sum");
+          const joinedHash = hash(modules.map(m => m.id).join("_"));
+          let len = nameLength;
+          while (seen.has(joinedHash.substr(0, len))) len++;
+          seen.add(joinedHash.substr(0, len));
+          return `chunk-${joinedHash.substr(0, len)}`;
+        } else {
+          return modules[0].id;
+        }
+      }),
+      // keep module.id stable when vender modules does not change
+      new webpack.HashedModuleIdsPlugin(),
+      // copy custom static assets
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, "../static"),
+          to: config.build.assetsSubDirectory,
+          ignore: [".*"]
+        }
+      ])
+    ]),
   // .concat(utils.htmlPlugin())
-  ,
   optimization: {
     splitChunks: {
       chunks: "all",
@@ -180,3 +204,78 @@ if (config.build.generateAnalyzerReport || config.build.bundleAnalyzerReport) {
 }
 
 module.exports = webpackConfig;
+
+ function htmlPlugin1() {
+  let entryHtml = glob.sync(PAGE_PATH + "/*/*.html");
+  let arr = [];
+  entryHtml.forEach(filePath => {
+    let filename = filePath.substring(
+      filePath.lastIndexOf("/") + 1,
+      filePath.lastIndexOf(".")
+    );
+    let conf = {
+      filename: path.resolve(__dirname, "../dist/" + filename + ".html"),
+      template: path.resolve(
+        __dirname,
+        "../src/pages/" + filename + "/" + filename + ".html"
+      ),
+      // 页面模板需要加对应的js脚本，如果不加这行则每个页面都会引入所有的js脚本
+      chunks: [filename, "chunk-libs", "chunk-elementUI"],
+      title: filename,
+      inject: true
+    };
+    if (process.env.NODE_ENV === "production") {
+      conf = merge(conf, {
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+        }
+        // chunksSortMode: 'dependency'
+      });
+    }
+    arr.push(new HtmlWebpackPlugin(conf));
+  });
+  return arr;
+};
+
+ function htmlPlugin2() {
+  return [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, "../dist/index.html"),
+      template: path.resolve(__dirname, "../src/pages/index/index.html"),
+      inject: true,
+      favicon: resolve("favicon.ico"),
+      title: "index",
+      chunks: ["index", "chunk-libs", "chunk-elementUI"],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      }
+      // default sort mode uses toposort which cannot handle cyclic deps
+      // in certain cases, and in webpack 4, chunk order in HTML doesn't
+      // matter anyway
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, "../dist/cell.html"),
+      template: path.resolve(__dirname, "../src/pages/cell/cell.html"),
+      inject: true,
+      favicon: resolve("favicon.ico"),
+      title: "cell",
+      chunks: ["cell", "chunk-libs", "chunk-elementUI"],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      }
+      // default sort mode uses toposort which cannot handle cyclic deps
+      // in certain cases, and in webpack 4, chunk order in HTML doesn't
+      // matter anyway
+    })
+  ];
+};
